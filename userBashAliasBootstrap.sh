047e87d018274457d2e59a02886bfd1a04b534c4
Robin Melanson <robin.e.melanson@gmail.com>
2022-01-20 18:51:06 -0500

    Create userBashAliasBootstrap.sh

---
+++ userBashAliasBootstrap.sh
@@ -0,0 +1,23 @@
+echoLog(){
+ echo $* 2>&1 | tee -a setup.log
+}
+
+is_current_shell(){
+  shell=$0
+  echo shell = $shell
+  [ $shell = "-bash" ] || [ $shell = "bash" ]
+}
+
+if ! is_current_shell
+then {
+  echoLog ERROR: Not current shell
+  echoLog USAGE: . $0
+  echoLog EXITING . $0
+  exit
+}
+fi
+
+cp etc/profile.d/startUpAliasFiles/.ua ~/.a
+cp etc/profile.d/startUpAliasFiles/.um ~/.m
+. ~/.a
+. ~/.m
