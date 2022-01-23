24e92607225bf756278a929b3c987dbae4f0c509
EC2 Default User <ec2-user@ip-172-31-4-251.ca-central-1.compute.internal>
2022-01-23 01:17:35 +0000

    WIP

--- etc/profile.d/bashStartup.sh
+++ etc/profile.d/bashStartup.sh
@@ -11,18 +11,45 @@ echoLog(){
  echo $* 2>&1 | tee -a setup.log
 }
 
+strInFile(){
+   str=$1
+   fname=$2
+   echoLog Searching for String $str in File $fname
+   echoLog EXECUTING: cat $fname \| grep -o "$str" \| wc -l
+#   found=$(cat $fname | grep -c "$str")
+   found=$(cat $fname | grep -o "$str" | wc -l)
+   echoLog string found = $found
+  [ ! $found -eq 0 ]
+}
 
-if [ -d "$USER_DIR" ];
-then {
-    echo "DIRECTORY $USER_DIR exists."
+updateBashrc(){
+  if strInFile "$SET_ENV" ~/.bashrc
+   then
+     echoLog LAUNCH BASH ENVIRONMENT ALIASE\'S ALREADY INSTALLED
+  else
+    echoLog LAUNCH BASH ENVIRONMENT ALIASE\'S SETTINGS >> ~/.bashrc
+    echoLog EXECUTING: echo $SET_ENV \>\> ~/.bashrc
+    echoLog $SET_ENV >> ~/.bashrc
+  fi
 }
-else {
-    echoLog "DIRECTORY $SOURCE_DIR Not Found."
-    echoLog "EXECUTING: cp -rf $SOURCE_DIR $USER_DIR"
-    cp -rf $SOURCE_DIR $USER_DIR
-    echoLog 766 $USER_DIR
-    chmod 766 $USER_DIR
-    echoLog EXECUTING: $SET_ENV
-    $SET_ENV
+
+setBashEnv(){
+  if [ -d "$USER_DIR" ];
+    then {
+      echoLog "DIRECTORY $USER_DIR exists."
+      echoLog "SETTING ENV"
+    }
+    else {
+      echoLog "DIRECTORY $SOURCE_DIR Not Found."
+      echoLog "EXECUTING: cp -rf $SOURCE_DIR $USER_DIR"
+      cp -rf $SOURCE_DIR $USER_DIR
+      echoLog 766 $USER_DIR
+      chmod 766 $USER_DIR
+      updateBashrc
+      echoLog EXECUTING: $SET_ENV
+      $SET_ENV
+    }
+  fi
 }
-fi
+
+updateBashrc
